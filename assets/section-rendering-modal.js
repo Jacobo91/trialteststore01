412c7fa1c9bdf0a16212297c07c1ce9a45b97e68
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-01-15 12:36:30 -0500

    add section rendering logic

--- assets/section-rendering-modal.js
+++ assets/section-rendering-modal.js
@@ -1,15 +1,14 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const quickViewButtons = document.querySelectorAll('#quick-view-button');
-    const sectionId = 'template--18992521511170__product-grid';
-    const url = `${window.location.pathname}?section=${sectionId}&page=2`;
+document.addEventListener('DOMContentLoaded', () => {
+    const quickViewButtons = document.querySelectorAll('.quick-view-button');
+    const sectionId = document.getElementById('quick-view-modal').getAttribute('data-section');
     const quickViewModal = document.querySelector('#quick-view-modal');
 
     const fetchData = async (url) => {
         console.log('fetching data with url:', url);
         try {
             const response = await fetch(url);
-            const responseTxt = await response.text();
-            renderSection(responseTxt);
+            const responseText = await response.text();
+            renderSection(responseText);
         } catch (error) {
             
         }
@@ -18,18 +17,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const renderSection = (html) => {
         const newHTML = new DOMParser()
             .parseFromString(html, 'text/html')
-            .getElementById('ProductGridContainer').innerHTML;
+            .getElementById('quick-view-modal').innerHTML;
 
         console.log('new html is:', newHTML);
 
-        document.getElementById('ProductGridContainer').innerHTML = newHTML;
+        document.getElementById('quick-view-modal').innerHTML = newHTML;
     };
 
     quickViewButtons.forEach(quickViewButton => {
-        quickViewButton.addEventListener('click', () => {
+        quickViewButton.addEventListener('click', (event) => {
+            const productHandle = event.target.getAttribute('data-product-handle');
+            const productEndpoint = `${window.Shopify.routes.root}products/${productHandle}?section_id=${sectionId}`;
             console.log('clicking');
-            console.log('url is:', url);
-            // fetchData(url)
+            console.log('product url is:', productEndpoint);
+            fetchData(productEndpoint);
             if (quickViewModal.classList.contains('hide-modal')) {
                 quickViewModal.classList.remove('hide-modal');
                 quickViewModal.classList.add('show-modal');
