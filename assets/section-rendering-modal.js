501bab7a2480888ba487ad10532d479ddc9d594e
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-01-15 11:28:22 -0500

    add quick view modal section

--- assets/section-rendering-modal.js
+++ assets/section-rendering-modal.js
@@ -1,22 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
     const quickViewButtons = document.querySelectorAll('#quick-view-button');
     const sectionId = 'template--18992521511170__product-grid';
-    const url = `${window.location.pathname}?section=${sectionId}?page=2`;
+    const url = `${window.location.pathname}?section=${sectionId}&page=2`;
 
     const fetchData = async (url) => {
         console.log('fetching data with url:', url);
         try {
             const response = await fetch(url);
             const responseTxt = await response.text();
-            const newHTML = new DOMParser()
-                .parseFromString(responseTxt, 'text/html')
-                .getElementById('ProductGridContainer').innerHTML;
-            console.log('new html is:', newHTML);
-
-            document.getElementById('ProductGridContainer').innerHTML = newHTML;
+            renderSection(responseTxt);
         } catch (error) {
             
         }
+    };
+
+    const renderSection = (html) => {
+        const newHTML = new DOMParser()
+            .parseFromString(html, 'text/html')
+            .getElementById('ProductGridContainer').innerHTML;
+            
+        console.log('new html is:', newHTML);
+
+        document.getElementById('ProductGridContainer').innerHTML = newHTML;
     };
 
     quickViewButtons.forEach(quickViewButton => {
