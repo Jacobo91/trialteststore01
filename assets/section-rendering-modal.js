72e8ca140264af7caabc22e700ab9a7070e1ca9b
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-01-15 11:44:32 -0500

    add css and section id

--- assets/section-rendering-modal.js
+++ assets/section-rendering-modal.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const quickViewButtons = document.querySelectorAll('#quick-view-button');
     const sectionId = 'template--18992521511170__product-grid';
     const url = `${window.location.pathname}?section=${sectionId}&page=2`;
+    const quickViewModal = document.querySelector('#quick-view-modal');
 
     const fetchData = async (url) => {
         console.log('fetching data with url:', url);
@@ -18,7 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
         const newHTML = new DOMParser()
             .parseFromString(html, 'text/html')
             .getElementById('ProductGridContainer').innerHTML;
-            
+
         console.log('new html is:', newHTML);
 
         document.getElementById('ProductGridContainer').innerHTML = newHTML;
@@ -28,7 +29,18 @@ window.addEventListener('DOMContentLoaded', () => {
         quickViewButton.addEventListener('click', () => {
             console.log('clicking');
             console.log('url is:', url);
-            fetchData(url)
+            // fetchData(url)
+            if (quickViewModal.classList.contains('hide-modal')) {
+                quickViewModal.classList.remove('hide-modal');
+                quickViewModal.classList.add('show-modal');
+            }
         });
+    });
+
+    quickViewModal.addEventListener('click', (event) => {
+        if (event.target.classList.contains('show-modal')) {
+            event.target.classList.remove('show-modal');
+            event.target.classList.add('hide-modal');
+        }
     });
 });
