03830635bfe309e7b50c61e233bb3ece3be84697
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-09-04 15:24:13 -0500

    aaaa

--- assets/shop-style.js
+++ assets/shop-style.js
@@ -10,21 +10,7 @@ class ShopStyle extends HTMLElement {
     console.log("Custom element added to page.");
   }
 
-  disconnectedCallback() {
-    console.log("Custom element removed from page.");
-  }
 
-  connectedMoveCallback() {
-    console.log("Custom element moved with moveBefore()");
-  }
-
-  adoptedCallback() {
-    console.log("Custom element moved to new page.");
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    console.log(`Attribute name has changed.`);
-  }
 }
 
 customElements.define("shop-style", ShopStyle);
