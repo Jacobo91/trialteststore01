52fbd7d3904003441f593fbd58810ec7c2b239d2
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-09-04 14:26:33 -0500

    add shop style skeletone

---
+++ assets/shop-style.js
@@ -0,0 +1,30 @@
+// Create a class for the element
+class ShopStyle extends HTMLElement {
+  static observedAttributes = ["color", "size"];
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    console.log("Custom element added to page.");
+  }
+
+  disconnectedCallback() {
+    console.log("Custom element removed from page.");
+  }
+
+  connectedMoveCallback() {
+    console.log("Custom element moved with moveBefore()");
+  }
+
+  adoptedCallback() {
+    console.log("Custom element moved to new page.");
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute name has changed.`);
+  }
+}
+
+customElements.define("shop-style", ShopStyle);
